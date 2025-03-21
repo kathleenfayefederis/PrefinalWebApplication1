bb266c0afd8e1dffb39c25e57746d7949eb4baca
COMLAB507 <COMLAB507@DESKTOP-BD2DG6S>
2025-03-21 10:56:53 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PrefinalWebApplication1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
